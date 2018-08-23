2a5dad3c7ec29bf117268f40dc16c85df7ee1128
Brian <riceb53@gmail.com>
2018-08-22 19:23:31 -0500

    add calculator.rb

---
+++ calculator.rb
@@ -0,0 +1,27 @@
+class Calculator
+  def add(number_one, number_two)
+    return number_one + number_two
+  end
+
+  def subtract(number_one, number_two)
+    return number_one - number_two
+  end
+
+  def multiply(number_one, number_two)
+    return number_one * number_two
+  end
+
+  def divide(dividend, divisor)
+    return dividend / divisor
+  end
+
+  def sqaure(number)
+    return square * square
+  end
+
+  def power(number, exponent)
+    return number ** exponent
+  end
+end
+
+
